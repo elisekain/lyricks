67932d7f1a87899af671ed7dfed9fc4a7e46d4a8
Elise Kain <elise.kain@gmail.com>
2015-11-17 18:59:32 -0500

    Controller refactor

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,14 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:show, :edit, :update, :destroy, :add_tag, :remove_tag]
+  before_action :set_tags, only: [:index, :browse, :show, :sort, :sort_user]
+  before_action :set_tag, only: [:add_tag, :remove_tag, :sort, :sort_user]
 
   def index
     @cards = current_user.cards
-    @tags = Tag.all
   end
 
   def browse
     @cards = Card.all
-    @tags = Tag.all
   end
 
   def new
@@ -25,8 +26,6 @@ class CardsController < ApplicationController
   end
 
   def show
-    @card = Card.find(params[:id])
-    @tags = Tag.all
     @text_collection = @card.lyrics
     while @text_collection.length < 2400
       @text_collection += @card.lyrics
@@ -34,11 +33,9 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def update
-    @card = Card.find(params[:id])
     if @card.update(card_params)
       flash[:notice] = "#{@card.title} was successfully updated."
       redirect_to card_path(@card)
@@ -48,45 +45,49 @@ class CardsController < ApplicationController
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.destroy
     redirect_to cards_path
   end
 
   def add_tag
-    @card = Card.find(params[:id])
-    @tag = Tag.find(params[:tag_id])
     @card.card_tags.create(tag: @tag)
     redirect_to card_path(@card)
   end
   #
   # remove tag
   def remove_tag
-    @card = Card.find(params[:id])
-    @tag = Tag.find(params[:tag_id])
     @card.card_tags.where(tag: @tag).destroy_all
     redirect_to card_path(@card)
   end
 
   # sort all by tag
   def sort
-    @sort_tag = Tag.find(params[:tag_id])
     @cards = Card.includes(:tags).where('tags.title = ?', @sort_tag.title).references(:tags)
-    @tags = Tag.all
     render :browse
   end
 
   # sort user's by tag
   def sort_user
-    @sort_tag = Tag.find(params[:tag_id])
     @cards = Card.includes(:tags).where('tags.title = ?', @sort_tag.title).references(:tags)
     @cards = @cards.select { |card|  card.user == current_user  }
-    @tags = Tag.all
     render :index
   end
 
   private
   def card_params
     params.require(:card).permit(:title, :lyrics, :icon_selection, :color_selection, :font_selection, :private)
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def set_tags
+    @tags = Tag.all
+  end
+
+  def set_tag
+    @tag = Tag.find(params[:tag_id])
+    @sort_tag = Tag.find(params[:tag_id])
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ActiveRecord::Base
   belongs_to :user
-  has_many :card_tags
+  has_many :card_tags, dependent: :destroy
   has_many :tags, through: :card_tags
 end
